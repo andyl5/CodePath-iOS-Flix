cdf4dde8d5d1b28b0c6da3688425c61684151aab
speedpick6 <81489476+speedpick6@users.noreply.github.com>
2021-09-25 23:18:49 -0400

    Added following functionalities: Deatiled cell view for particular movies, Tab bar button to view grid layout of movies

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by andy on 9/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
